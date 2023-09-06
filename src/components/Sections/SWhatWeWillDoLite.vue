3240386d1080eb0011aea953b36dfff6859cf450
Xavi Torelló <info@xaviertorello.cat>
2023-09-06 19:29:51 +0200

    Add initial lite WWDo

---
+++ src/components/Sections/SWhatWeWillDoLite.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import ATitle from '../Atoms/ATitle.vue'
+import ALink from '../Atoms/ALink.vue'
+import { startTime, dueTime, eventDate } from '../../content'
+import ASection from '../Atoms/ASection.vue'
+</script>
+
+<template>
+  <a-section>
+    <a-title>Què farem?</a-title>
+    <p class="mb-4">
+      Serà una trobada presencial a Girona el <b>{{ eventDate }}</b>. En breu confirmarem el lloc i l'agenda.
+    </p>
+    <p class="mb-4">
+      Segurament començarem a les <b>{{ startTime }}</b> i finalitzarem a les <b>{{ dueTime }}</b>, amb una pausa per dinar.
+    </p>
+    <p class="font-bold">Marató de PRs</p>
+    <p class="mb-4">
+      S'habilitarà un espai per a qui vulgui participar a la marató de PRs. Els grups, o equips, s'organitzaran insitu,
+      tot hi que si algú vol fer-ho sol, total llibertat.
+    </p>
+    <p class="mb-4">
+      Hi haurà un grup de mentors que ajudaran a dinamitzar l'esdeveniment, donant suport o proposant repos on
+      contribuir-hi.
+    </p>
+    <p class="mb-4">La idea principal és passar-nos-ho bé i si a més aconseguim fer contribucions, més que millor!</p>
+  </a-section>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@ import { IMenuOption } from '../types'
 import SMenu from '../components/Sections/SMenu.vue'
 import SIntro from '../components/Sections/SIntro.vue'
 import SJoin from '../components/Sections/SJoin.vue'
-import SWhatWeWillDo from '../components/Sections/SWhatWeWillDo.vue'
+import SWhatWeWillDoLite from '../components/Sections/SWhatWeWillDoLite.vue'
 import SDiary from '../components/Sections/SDiary.vue'
 import SCodeOfConduct from '../components/Sections/SCodeOfConduct.vue'
 import SOrganizers from '../components/Sections/SOrganizers.vue'
@@ -27,9 +27,9 @@ const handlerMenuClick = (option: IMenuOption) => {
 <template>
   <s-menu @click="handlerMenuClick" id="begin" />
   <s-intro id="intro" />
+  <s-what-we-will-do-lite id="watWeWillDo" />
 <!-- 
   <s-join id="join" />
-  <s-what-we-will-do id="watWeWillDo" />
   <s-diary id="diary" />
   <s-speakers />
 -->
